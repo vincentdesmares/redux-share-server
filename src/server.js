6009bf0913ae241466a813207fcce90372184e5b
Baptiste Manson <baptiste.manson@inovia.fr>
2016-03-12 17:03:24 -0800

    Server -> client.

--- src/server.js
+++ src/server.js
@@ -6,20 +6,27 @@ var server = require('http').createServer()
   , app = express()
   , port = 2000;
 
+var bodyParser = require('body-parser')
 
 import { createStore,applyMiddleware } from 'redux'
 import {reducer, actions} from './actions.js'
-
 var store = createStore(reducer);
 
-app.use(function (req, res) {
-  res.send({ msg: "hello man" });
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json())
+
+app.post('/dispatch-to-all', function (req, res) {
+  console.log("Dispatching an action to all",req.body);      // your JSON
+  wss.clients.forEach(function each(client) {
+    client.send(JSON.stringify(req.body));
+  });
+  res.send("DONE");
 });
+
 
 wss.on('connection', function connection(ws) {
   var location = url.parse(ws.upgradeReq.url, true);
-  // you might use location.query.access_token to authenticate or share sessions
-  // or ws.upgradeReq.headers.cookie (see http://stackoverflow.com/a/16395220/151312)
 
   ws.on('message', function incoming(message) {
     console.log(message);
--- src/sync-store.js
+++ src/sync-store.js
@@ -8,14 +8,17 @@ export default class SyncStore {
 		this.ws = new WebSocket(this.url);
 		this.store = store;
 		this.ws.onopen = function() {
-			console.log("connected!");
+			console.log("Sync connected!");
 			//send a state dump
 			this.readyToSend = true;
 			let state = this.store.getState() || {};
 			this.store.dispatch({type:"@@SYNC-SERVER-DUMP",state:state});
 		}.bind(this);
 
-		this.ws.onmessage =  event => console.log("Received some stuff",event.data);
+		this.ws.onmessage =  event => {
+			console.log("Sync: Received some stuff from the server",event.data);
+			this.store.dispatch(JSON.parse(event.data));
+		}
 		this.ws.onclose = () => setTimeout(this.init.bind(this),1000);
 	}
 
@@ -26,10 +29,10 @@ export default class SyncStore {
 	getClientMiddleware() {
 		return store => next => action => {
   				//need to enrich next action.
-  				console.log('dispatching ', action)
+  				console.log('Sync: dispatching ', action)
   				let result = next(action)
   				if(this.readyToSend) this.send(action);
-  				if(action.type ==="@@SYNC-CONNECT") this.init(store);
+  				if(action.type === "@@SYNC-CONNECT") this.init(store);
   				return result;
 		}
 	}
