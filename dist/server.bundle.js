b4bdaa4b8cf814c5f60dec4739b22167b710aa85
Vincent Desmares <vincent.desmares@gmail.com>
2016-03-17 23:59:20 -0700

    cleanup

--- dist/server.bundle.js
+++ dist/server.bundle.js
@@ -65,32 +65,104 @@ return /******/ (function(modules) { // webpackBootstrap
 	var express = __webpack_require__(3);
 
 	var SyncReduxServer = function () {
+	  /**
+	   * Constructor of the server
+	   *
+	   * @param store
+	   * @param server
+	   */
+
 	  function SyncReduxServer(store, server) {
+	    var broadcastOnReceive = arguments.length <= 2 || arguments[2] === undefined ? false : arguments[2];
+	    var onConnectionCallback = arguments.length <= 3 || arguments[3] === undefined ? null : arguments[3];
+	    var onPreSendCallback = arguments.length <= 4 || arguments[4] === undefined ? null : arguments[4];
+	    var onActionReceived = arguments.length <= 5 || arguments[5] === undefined ? null : arguments[5];
+
 	    _classCallCheck(this, SyncReduxServer);
 
+	    /**
+	     * Websocket Server
+	     */
 	    this.wss = new WebSocketServer({ server: server });
+
+	    /**
+	     * Redux store to link to the clients
+	     */
 	    this.store = store;
-	    this.broadcastOnReceive = false;
+
+	    /**
+	     * If true, will broadcast received action to all clients
+	     * @type {boolean}
+	     */
+	    this.broadcastOnReceive = broadcastOnReceive;
+
+	    /**
+	     * Flag to know if the server must display debug
+	     *
+	     * @type {boolean}
+	     */
 	    this.debug = false;
+
+	    /**
+	     * After the connection of a new client,
+	     * this function will be called if set
+	     *
+	     * @type {null}
+	     */
+	    this.onConnectionCallback = onConnectionCallback;
+
+	    /**
+	     * If set, will be called before sending a message
+	     * If it return false the message will not be set
+	     * @type {function(socket)}
+	     */
+	    this.onPreSendCallback = onPreSendCallback;
+
+	    /**
+	     * If set, will be called just after receiving an action
+	     *
+	     * @type {function(action, socket)}
+	     */
+	    this.onActionReceived = onActionReceived;
+
+	    /**
+	     * Bind the socket behavior
+	     */
 	    this.wss.on('connection', function connection(socket) {
-	      socket.id = Math.random() * 9999999 + '';
+	      if (this.onConnectionCallback !== null) {
+	        this.onConnectionCallback(socket);
+	      }
 	      if (this.debug) {
 	        console.log('Assigned id: ' + socket.id);
 	      }
 	      socket.on('message', function incoming(message) {
-	        console.log(message);
+	        if (this.debug) {
+	          console.log(message);
+	        }
 	        var action = JSON.parse(message);
-	        action.senderId = socket.id;
+	        if (this.onActionReceived !== null) {
+	          action = this.onActionReceived.apply(this, [action, socket]);
+	        }
+	        if (this.debug) {
+	          console.log('Action to dispatch: [%o]', action);
+	        }
 	        this.store.dispatch(action);
 	        if (this.debug) {
 	          console.log(store.getState());
 	        }
 	        if (this.broadcastOnReceive) {
-	          this.broadcastToOtherClient(action, socket);
+	          this.broadcastToOtherClient(action);
 	        }
 	      }.bind(this));
 	    }.bind(this));
 	  }
+
+	  /**
+	   * Set the debug mode of the server
+	   *
+	   * @param debug
+	   */
+
 
 	  _createClass(SyncReduxServer, [{
 	    key: 'setDebug',
@@ -99,11 +171,13 @@ return /******/ (function(modules) { // webpackBootstrap
 
 	      this.debug = debug;
 	    }
-	  }, {
-	    key: 'setBroadcastOnReceive',
-	    value: function setBroadcastOnReceive(broadcast) {
-	      this.broadcastOnReceive = broadcast;
-	    }
+
+	    /**
+	     * Return an express middleware
+	     *
+	     * @returns {*}
+	     */
+
 	  }, {
 	    key: 'getMiddleware',
 	    value: function getMiddleware() {
@@ -128,14 +202,62 @@ return /******/ (function(modules) { // webpackBootstrap
 
 	      return router;
 	    }
+
+	    /**
+	     * Send an action
+	     * @param socket
+	     * @param action
+	     */
+
+	  }, {
+	    key: 'sendAction',
+	    value: function sendAction(action, socket) {
+	      if (this.onPreSendCallback !== null) {
+	        if (!this.onPreSendCallback.apply(this, [action, socket])) {
+	          return;
+	        }
+	      }
+	      socket.send(JSON.stringify(action));
+	    }
+
+	    /**
+	     * Find a socket searching for a property
+	     * Return the last socket matching the property
+	     *
+	     * return null in case no socket are found
+	     * @param value
+	     * @param property
+	     * @returns {socket|null}
+	     */
+
+	  }, {
+	    key: 'getSocketByProperty',
+	    value: function getSocketByProperty(value, property) {
+	      var socketMatch = null;
+	      this.wss.clients.forEach(function each(socket) {
+	        if (socket[property] !== undefined && socket[property] === value) {
+	          socketMatch = socket;
+	        }
+	      }.bind(this));
+	      if (socketMatch === null && this.debug) {
+	        console.log("Did not found any socket with [" + property + "] to [" + value + "]");
+	      }
+	      return socketMatch;
+	    }
+
+	    /**
+	     * Broadcast a message to all clients
+	     *
+	     * @param action
+	     * @param senderSocket
+	     */
+
 	  }, {
 	    key: 'broadcastToOtherClient',
-	    value: function broadcastToOtherClient(action, senderSocket) {
-	      this.wss.clients.forEach(function each(client) {
-	        if (client.id !== senderSocket.id && action.type !== '@@SYNC-CONNECT-SERVER-END') {
-	          client.send(JSON.stringify(action));
-	        }
-	      });
+	    value: function broadcastToOtherClient(action) {
+	      this.wss.clients.forEach(function each(socket) {
+	        this.sendAction(action, socket);
+	      }.bind(this));
 	    }
 	  }]);
 
@@ -145,7 +267,12 @@ return /******/ (function(modules) { // webpackBootstrap
 	;
 
 	module.exports = function (store, server) {
-	  return new SyncReduxServer(store, server);
+	  var broadcastOnReceive = arguments.length <= 2 || arguments[2] === undefined ? false : arguments[2];
+	  var onConnectionCallback = arguments.length <= 3 || arguments[3] === undefined ? null : arguments[3];
+	  var onPreSendCallback = arguments.length <= 4 || arguments[4] === undefined ? null : arguments[4];
+	  var onActionReceived = arguments.length <= 5 || arguments[5] === undefined ? null : arguments[5];
+
+	  return new SyncReduxServer(store, server, broadcastOnReceive, onConnectionCallback, onPreSendCallback, onActionReceived);
 	};
 
 /***/ },
